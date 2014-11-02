04bd181ff2b61a912ccc6680175c7e3ec846b52c
Tim zhang <changchengdecao@126.com>
2014-11-02 18:06:36 +0800

    Update imageCompress.py
    增加时间

--- imageCompress.py
+++ imageCompress.py
@@ -1,23 +1,34 @@
 #!/usr/bin/python
-#Filename imageCompress.py
+#Filename image_compress.py
 
 import os
 import sys
 import Image
+import time
 
 def resize(imgPath,width=700):
 	allowImageType = ['jpg','jpeg','gif','png']
-	if os.path.isfile(imgPath) and imgPath.split('.')[-1].lower() in allowImageType :
+	if imgPath.split('.')[-1].lower() in allowImageType :
 		img = Image.open(imgPath)
 		(w,h) = img.size
 		if w>width:
-			height = h * width / w
-			out = img.resize((width,height),Image.ANTIALIAS)
-			out.save(imgPath,quality=95)
+			try:
+				height = h * width / w
+				out = img.resize((width,height),Image.ANTIALIAS)
+				out.save(imgPath,quality=95)
+			except IOError,e:
+				print "error:",e
 
 
 imageFolder = "./"
+count = 0
+time.clock()
 for path, dirs, files in os.walk(imageFolder):
 	for itme in files:
-		imgPath = path+itme
-		resize(imgPath)
+		imgPath = path+"/"+itme
+		if os.path.isfile(imgPath):
+			count = count +1
+			#print imgPath
+			resize(imgPath)
+
+print "count:",count,",time:",time.clock(),"s"
